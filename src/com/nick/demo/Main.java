b2146c7d9db634f3ffbf31ef5609404bea42cab0
Nick <nick.ledin@hotmail.com>
2023-09-29 10:28:01 +0200

    Uppgift 1

---
+++ src/com/nick/demo/Main.java
@@ -0,0 +1,12 @@
+package com.nick.demo;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 9; i++) {
+            System.out.println("Loop: "+(i+1));
+
+            // I debug mode stannar den i loopen vid SOUT där jag satt breakpoint
+
+        }
+    }
+}
